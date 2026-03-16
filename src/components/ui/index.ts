87683f48c67a6cb7b56cb2938ca6f8ed0c32804e
Steven Koehl <spkoehl@gmail.com>
2026-03-15 17:26:19 -0700

    feat(ui): add barrel export for pixel UI components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,15 @@
+// src/components/ui/index.ts
+export { default as PixelButton } from './PixelButton'
+export type { PixelButtonProps } from './PixelButton'
+
+export { default as PixelCard } from './PixelCard'
+export type { PixelCardProps } from './PixelCard'
+
+export { default as PixelProgressBar } from './PixelProgressBar'
+export type { PixelProgressBarProps } from './PixelProgressBar'
+
+export { default as PixelBadge } from './PixelBadge'
+export type { PixelBadgeProps } from './PixelBadge'
+
+export { default as PixelModal } from './PixelModal'
+export type { PixelModalProps } from './PixelModal'
